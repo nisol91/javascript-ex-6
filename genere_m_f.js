fe93bb1a2ba0bf4a0d96876b28d22ebf6e0b7c40
nisol91 <nicolasolzi91@gmail.com>
2018-11-26 16:09:44 +0100

    2

--- genere_m_f.js
+++ genere_m_f.js
@@ -3,3 +3,16 @@
 // Sulla pagina html deve apparire “Ciao <nome>”, il
 // colore del nome deve essere azzurro o rosa a
 // seconda del sesso inserito
+var id = prompt('Inserisci nome');
+var sesso = prompt('Inserisci sesso');
+var testo = document.getElementById('text')
+testo.innerHTML = 'Ciao ';
+var nome = document.getElementById('name')
+if (sesso == 'maschio') {
+  // nome.style.color = 'lightblue';
+  nome.className = 'blue';
+} else if (sesso == 'femmina') {
+  // nome.style.color = 'pink';
+  nome.className = 'pink';
+}
+nome.innerHTML = id;
